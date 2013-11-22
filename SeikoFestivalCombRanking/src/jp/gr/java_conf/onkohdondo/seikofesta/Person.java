a91b06365252ccf0dab2b99f6ca7bc839e95c284
OnkohDondo <ideryuhouu@gmail.com>
2013-11-22 17:29:34 +0900

    参加者管理用クラスPersonの基本構造作成。

---
+++ SeikoFestivalCombRanking/src/jp/gr/java_conf/onkohdondo/seikofesta/Person.java
@@ -0,0 +1,58 @@
+package jp.gr.java_conf.onkohdondo.seikofesta;
+
+
+/**
+ * 参加者を管理するクラス。SuicaID、登録名、各種ゲーム上での点数が記録される。
+ * 
+ * @author Onkoh Dondo
+ *
+ */
+public class Person {
+	
+	/**
+	 * 参加者のSuica/PasmoのID番号（IDm）。<br>
+	 * コンストラクタ実行時に指定され、原則変更できない。
+	 * 
+	 * @see Person#getSuicaid()
+	 */
+	private int suicaid;
+	
+	/**
+	 * 参加者の名前。
+	 * 
+	 * @see Person#getName()
+	 */
+	private String name;
+	
+	public Person(int id,String name){
+		setSuicaid(id);
+		setName(name);
+	}
+	
+	/**
+	 * Suica/PasmoのID番号（IDm）を取得する。
+	 * 
+	 * @return　Suica/PasmoのIDm
+	 */
+	public int getSuicaid() {
+		return suicaid;
+	}
+
+	private void setSuicaid(int suicaid) {
+		this.suicaid = suicaid;
+	}
+
+	/**
+	 * 参加者の名前を取得する。
+	 * 
+	 * @return 参加者の名前
+	 */
+	public String getName() {
+		return name;
+	}
+
+	private void setName(String name) {
+		this.name = name;
+	}
+	
+}
