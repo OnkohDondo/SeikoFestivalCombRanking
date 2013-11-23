be07607f9ae90f23dccc0e9bda5014dfa50c864e
OnkohDondo <ideryuhouu@gmail.com>
2013-11-23 13:33:40 +0900

    表示用フレームを作成、MainProgramをTimerTask化、MainProgramからフレームの描画が一定間隔で行われるように設定

--- SeikoFestivalCombRanking/src/jp/gr/java_conf/onkohdondo/seikofesta/MainProgram.java
+++ SeikoFestivalCombRanking/src/jp/gr/java_conf/onkohdondo/seikofesta/MainProgram.java
@@ -1,21 +1,42 @@
 package jp.gr.java_conf.onkohdondo.seikofesta;
 
+import java.awt.Graphics2D;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
 import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
-public class MainProgram {
+public class MainProgram extends TimerTask{
 
 	private ArrayList<Game> games;
+	private RankingFrame rankingFrame;
 	
 	public static void main(String[] args) {
-		new MainProgram();
+		new Timer().schedule(new MainProgram(),300,50);
 	}
 	
 	private MainProgram(){
 		load();
+		rankingFrame=new RankingFrame(this);
+//		g=(Graphics2D) rankingFrame.getGraphics();
+		GraphicsDevice device =
+				GraphicsEnvironment.getLocalGraphicsEnvironment().
+				getDefaultScreenDevice();
+		device.setFullScreenWindow(rankingFrame);
 	}
 	
 	
 	private void load(){
 		games=new ArrayList<Game>();
+	}
+
+	@Override
+	public void run() {
+		rankingFrame.repaint();
+	}
+	
+	public void redraw(Graphics2D g){
+		g.drawString(System.currentTimeMillis()+"", 0, 100);
 	}
 }
